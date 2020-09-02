c2298f04ae4d139ff93287f2354f4c0566adc251
Chung Tran <bigearsenal@gmail.com>
2020-09-02 15:24:08 +0700

    prepare for ViewAnimation

--- CATest/ViewControllers/TableViewController.swift
+++ CATest/ViewControllers/TableViewController.swift
@@ -9,36 +9,41 @@
 import UIKit
 
 class TableViewController: UITableViewController {
-    lazy var viewControllers: [UIViewController] = [
-        LayerCustomTranstionVC(),
-        DisableImplicitAnimsVC(),
-        UsingPresentationLayerVC(),
-        CAAnimationDelegateVC(),
-        MovingAircraftVC(),
-        TransitionVC(),
-        KeyframeAnimationsVC(),
-        VirtualPropertyAnimsVC(),
-        AnimationGroupVC(),
-        TranstionUIKitVC(),
-        TransitionTabBarVC(),
-        RenderInContextVC(),
-        CancellableAnimationVC(),
-        SwingingDoorVC(),
-        TimeOffsetAndSpeedVC(),
-        PausingAnimation(),
-        EasingAnimationVC(),
-        EasingKeyframeAnimationVC(),
-        ClockVC(),
-        BoucingBallVC(),
-        BouncingBall2VC(),
-        BouncingFunctionVC(),
-        BouncingBallTimer(),
-        BouncingBallCADisplayLink(),
-        CustomDrawingVC<PathStrokeDrawingView>(),
-        CustomDrawingVC<LayerDrawingView>(),
-        ChalkBoardVC(),
-        ChalkBoardReducingDrawingVC(),
-        ImageCarouselVC()
+    lazy var viewControllers: [(String, [UIViewController])] = [
+        ("Layer animations", [
+            LayerCustomTranstionVC(),
+            DisableImplicitAnimsVC(),
+            UsingPresentationLayerVC(),
+            CAAnimationDelegateVC(),
+            MovingAircraftVC(),
+            TransitionVC(),
+            KeyframeAnimationsVC(),
+            VirtualPropertyAnimsVC(),
+            AnimationGroupVC(),
+            TranstionUIKitVC(),
+            TransitionTabBarVC(),
+            RenderInContextVC(),
+            CancellableAnimationVC(),
+            SwingingDoorVC(),
+            TimeOffsetAndSpeedVC(),
+            PausingAnimation(),
+            EasingAnimationVC(),
+            EasingKeyframeAnimationVC(),
+            ClockVC(),
+            BoucingBallVC(),
+            BouncingBall2VC(),
+            BouncingFunctionVC(),
+            BouncingBallTimer(),
+            BouncingBallCADisplayLink(),
+            CustomDrawingVC<PathStrokeDrawingView>(),
+            CustomDrawingVC<LayerDrawingView>(),
+            ChalkBoardVC(),
+            ChalkBoardReducingDrawingVC(),
+            ImageCarouselVC()
+        ].reversed()),
+        ("View animations", [
+            
+        ].reversed())
     ].reversed()
     
     override func viewDidLoad() {
@@ -49,25 +54,29 @@ class TableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return viewControllers.count
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return viewControllers.count
+        return viewControllers[section].1.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
 
         // Configure the cell...
-        cell.textLabel?.text = "\(type(of: viewControllers[indexPath.row]).self)"
+        cell.textLabel?.text = "\(type(of: viewControllers[indexPath.section].1[indexPath.row]).self)"
 
         return cell
     }
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        show(viewControllers[indexPath.row], sender: nil)
+        show(viewControllers[indexPath.section].1[indexPath.row], sender: nil)
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        viewControllers[section].0
     }
 
 }
---
+++ CATest/ViewControllers/UIViewAnimations/CrossfadingAnimationVC.swift
@@ -0,0 +1,9 @@
+//
+//  CrossfadingAnimationVC.swift
+//  CATest
+//
+//  Created by Chung Tran on 9/2/20.
+//  Copyright © 2020 Chung Tran. All rights reserved.
+//
+
+import Foundation
